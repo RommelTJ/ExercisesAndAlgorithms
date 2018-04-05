5c5b2e337068bde32cdf16d292b2e7d6cd0546eb
Rommel Rico <me@rommelrico.com>
2018-04-05 08:56:14 -0700

    Solution comments

--- java/src/com/rommelrico/exercises/q4highestproductofthree/HighestProductOfThree.java
+++ java/src/com/rommelrico/exercises/q4highestproductofthree/HighestProductOfThree.java
@@ -1,5 +1,62 @@
 package com.rommelrico.exercises.q4highestproductofthree;
 
+/**
+ * Solution
+ *
+ * We use a greedy approach to solve the problem in one pass. At each iteration
+ * we keep track of:
+ *
+ *     highestProductOf3
+ *     highestProductOf2
+ *     highest
+ *     lowestProductOf2
+ *     lowest
+ *
+ * When we reach the end, the highestProductOf3 is our answer. We maintain the
+ * others because they're necessary for keeping the highestProductOf3 up to
+ * date as we walk through the array. At each iteration, the highestProductOf3
+ * is the highest of:
+ *
+ *     the current highestProductOf3,
+ *     current * highestProductOf2,
+ *     current * lowestProductOf2 (if current and lowestProductOf2 are both
+ *     low negative numbers, this product is a high positive number).
+ *
+ * Complexity
+ *
+ * O(n) time and O(1) additional space.
+ *
+ * What We Learned
+ *
+ * Greedy algorithms in action again!
+ * That's not a coincidence—to illustrate how one pattern can be used to break
+ * down several different questions, we're showing this one pattern in action
+ * on several different questions.
+ *
+ * Usually it takes seeing an algorithmic idea from a few different angles for
+ * it to really make intuitive sense.
+ *
+ * Our goal here is to teach you the right way of thinking to be able to break
+ * down problems you haven't seen before. Greedy algorithm design is a big part
+ * of that way of thinking.
+ *
+ * For this one, we built up our greedy algorithm exactly the same way we did
+ * for the Apple stocks question. By asking ourselves:
+ * "Suppose we could come up with the answer in one pass through the input, by
+ * simply updating the 'best answer so far' as we went. What additional values
+ * would we need to keep updated as we looked at each item in our set, in order
+ * to be able to update the 'best answer so far' in constant time?"
+ *
+ * For the Apple stocks question, the only "additional value" we needed was the
+ * min price so far.
+ * For this one, we needed four things in order to calculate the new
+ * highestProductOf3 at each step:
+ *      highestProductOf2
+ *      highest
+ *      lowestProductOf2
+ *      lowest
+ * 
+ */
 public class HighestProductOfThree {
     private static int[] testArray = new int[] {1, 10, -5, 1, -100};
 
