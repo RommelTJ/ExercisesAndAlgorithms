9296052d8f49a18f610e81f8941d3629922525f9
Rommel Rico <rommeltj@gmail.com>
2018-04-22 10:46:26 -0700

    sampleHashFile() implementation

--- java/src/com/rommelrico/exercises/q43findduplicatefiles/FindDuplicateFiles.java
+++ java/src/com/rommelrico/exercises/q43findduplicatefiles/FindDuplicateFiles.java
@@ -1,7 +1,13 @@
 package com.rommelrico.exercises.q43findduplicatefiles;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.nio.file.Path;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
@@ -14,8 +20,28 @@ public class FindDuplicateFiles {
     }
 
     private static String sampleHashFile(Path path) throws IOException, NoSuchAlgorithmException {
-        // TODO: Implement me.
-        return null;
+        final long totalBytes = new File(path.toString()).length();
+
+        try(InputStream inputStream = new FileInputStream(path.toString())) {
+            MessageDigest digest = MessageDigest.getInstance("SHA-512");
+            DigestInputStream digestInputStream = new DigestInputStream(inputStream, digest);
+
+            // if the file is too short to take 3 samples, hash the entire file
+            if (totalBytes < SAMPLE_SIZE * 3) {
+                byte[] bytes = new byte[(int) totalBytes];
+                digestInputStream.read(bytes);
+            } else {
+                byte[] bytes = new byte[SAMPLE_SIZE * 3];
+                long numBytesBetweenSamples = (totalBytes - SAMPLE_SIZE * 3) / 2;
+
+                // read first, middle and last bytes
+                for (int n = 0; n < 3; n++) {
+                    digestInputStream.read(bytes, n * SAMPLE_SIZE, SAMPLE_SIZE);
+                    digestInputStream.skip(numBytesBetweenSamples);
+                }
+            }
+            return new BigInteger(1, digest.digest()).toString(16);
+        }
     }
 
     public static void main(String[] args) {
