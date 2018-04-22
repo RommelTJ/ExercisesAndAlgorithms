2453905a163c367209b7cdb0c65f5410c27fa7e7
Rommel Rico <rommeltj@gmail.com>
2018-04-22 10:48:22 -0700

    findDuplicateFiles() implementation

--- java/src/com/rommelrico/exercises/q43findduplicatefiles/FindDuplicateFiles.java
+++ java/src/com/rommelrico/exercises/q43findduplicatefiles/FindDuplicateFiles.java
@@ -9,14 +9,81 @@ import java.nio.file.Path;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.ArrayList;
 
 public class FindDuplicateFiles {
     private static final int SAMPLE_SIZE = 4000;
 
     public static List<FilePaths> findDuplicateFiles(Path startingDirectory) {
-        // TODO: Implement me.
-        return null;
+        Map<String, FileInfo> filesSeenAlready = new HashMap<>();
+        Stack<Path> stack = new Stack<>();
+        stack.push(startingDirectory);
+
+        List<FilePaths> duplicates = new ArrayList<>();
+
+        while (!stack.empty()) {
+
+            Path currentPath = stack.pop();
+            File currentFile = new File(currentPath.toString());
+
+            // if it's a directory,
+            // put the contents in our stack
+            if (currentFile.isDirectory()) {
+                for (File file : currentFile.listFiles()) {
+                    stack.add(file.toPath());
+                }
+
+                // if it's a file
+            } else {
+
+                // get its hash
+                String fileHash;
+                try {
+                    fileHash = sampleHashFile(currentPath);
+                } catch (IOException | NoSuchAlgorithmException e) {
+
+                    // show error and skip this file
+                    e.printStackTrace();
+                    continue;
+                }
+
+                // get its last edited time
+                long currentLastEditedTime = currentFile.lastModified();
+
+                // if we've seen it before
+                if (filesSeenAlready.containsKey(fileHash)) {
+
+                    FileInfo fileInfo = filesSeenAlready.get(fileHash);
+                    long existingLastEditedTime = fileInfo.timeLastEdited;
+                    Path existingPath = fileInfo.path;
+
+                    if (currentLastEditedTime > existingLastEditedTime) {
+
+                        // current file is the dupe!
+                        duplicates.add(new FilePaths(currentPath, existingPath));
+
+                    } else {
+
+                        // old file is the dupe!
+                        duplicates.add(new FilePaths(existingPath, currentPath));
+
+                        // but also update filesSeenAlready to have the new file's info
+                        filesSeenAlready.put(fileHash, new FileInfo(currentLastEditedTime, currentPath));
+                    }
+
+                    // if it's a new file, throw it in filesSeenAlready
+                    // and record its path and last edited time,
+                    // so we can tell later if it's a dupe
+                } else {
+                    filesSeenAlready.put(fileHash, new FileInfo(currentLastEditedTime, currentPath));
+                }
+            }
+        }
+        return duplicates;
     }
 
     private static String sampleHashFile(Path path) throws IOException, NoSuchAlgorithmException {
