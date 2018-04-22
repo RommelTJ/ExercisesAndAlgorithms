68db54af7ece6d3faed30c1c4e2e5b01c25b2555
Rommel Rico <rommeltj@gmail.com>
2018-04-22 09:50:53 -0700

    FindDuplicateFiles scaffolding

---
+++ java/src/com/rommelrico/exercises/q43findduplicatefiles/FindDuplicateFiles.java
@@ -0,0 +1,24 @@
+package com.rommelrico.exercises.q43findduplicatefiles;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+public class FindDuplicateFiles {
+    private static final int SAMPLE_SIZE = 4000;
+
+    public static List<FilePaths> findDuplicateFiles(Path startingDirectory) {
+        // TODO: Implement me.
+        return null;
+    }
+
+    private static String sampleHashFile(Path path) throws IOException, NoSuchAlgorithmException {
+        // TODO: Implement me.
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO: Implement me.
+    }
+}
