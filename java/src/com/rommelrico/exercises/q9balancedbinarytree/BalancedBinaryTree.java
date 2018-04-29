e9e02f7a453e374cc29b08158c962ae03eb565cb
Rommel Rico <rommeltj@gmail.com>
2018-04-29 13:39:15 -0700

    Comments

--- java/src/com/rommelrico/exercises/q9balancedbinarytree/BalancedBinaryTree.java
+++ java/src/com/rommelrico/exercises/q9balancedbinarytree/BalancedBinaryTree.java
@@ -4,6 +4,41 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * Solution
+ *
+ * We do a depth-first walk through our tree, keeping track of the depth as we go. When we find a leaf, we add its
+ * depth to a list of depths if we haven't seen that depth already.
+ *
+ * Each time we hit a leaf with a new depth, there are two ways that our tree might now be unbalanced:
+ * 1. There are more than 2 different leaf depths
+ * 2. There are exactly 2 leaf depths and they are more than 1 apart.
+ *
+ * Why are we doing a depth-first walk and not a breadth-first one? You could make a case for either. We chose
+ * depth-first because it reaches leaves faster, which allows us to short-circuit earlier in some cases.
+ *
+ * Complexity
+ *
+ * O(n) time and O(n) space.
+ *
+ * What We Learned
+ *
+ * This is an intro to some tree basics. If this is new to you, don't worry—it can take a few questions for this
+ * stuff to come together. We have a few more coming up.
+ *
+ * Particular things to note:
+ *
+ * Focus on depth-first vs breadth-first traversal. You should be very comfortable with the differences between the
+ * two and the strengths and weaknesses of each.
+ *
+ * You should also be very comfortable coding each of them up.
+ *
+ * One tip: Remember that breadth-first uses a queue and depth-first uses a stack (could be the call stack or an
+ * actual stack object). That's not just a clue about implementation, it also helps with figuring out the differences
+ * in behavior. Those differences come from whether we visit nodes in the order we see them (first in, first out) or we
+ * visit the last-seen node first (last in, first out).
+ *
+ */
 public class BalancedBinaryTree {
 
     public static boolean isBalanced(BinaryTreeNode treeRoot) {
