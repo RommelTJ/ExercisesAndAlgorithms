a7b7849c1387c89e191b94b812ba15e8746d7872
Rommel Rico <rommeltj@gmail.com>
2018-05-13 08:34:54 -0700

    Solution

--- java/src/com/rommelrico/exercises/q11secondlargestiteminbst/Find2ndLargestInBST.java
+++ java/src/com/rommelrico/exercises/q11secondlargestiteminbst/Find2ndLargestInBST.java
@@ -2,18 +2,41 @@ package com.rommelrico.exercises.q11secondlargestiteminbst;
 
 public class Find2ndLargestInBST {
 
-    public static int findLargest(BinaryTreeNode rootNode) {
+    private static int findLargest(BinaryTreeNode rootNode) {
         BinaryTreeNode current = rootNode;
-        BinaryTreeNode largest = null;
-
-        while (current != null) {
-            if (current.right == null) {
-                largest = current;
-            }
+        while (current.right != null) {
             current = current.right;
         }
-        return largest.value;
+        return current.value;
     }
 
-    
+    public static int findSecondLargest(BinaryTreeNode rootNode) {
+        if (rootNode == null || (rootNode.left == null
+                && rootNode.right == null)) {
+            throw new IllegalArgumentException("Tree must have at least 2 nodes");
+        }
+
+        BinaryTreeNode current = rootNode;
+
+        while (true) {
+
+            // case: current is largest and has a left subtree
+            // 2nd largest is the largest in that subtree
+            if (current.left != null && current.right == null) {
+                return findLargest(current.left);
+            }
+
+            // case: current is parent of largest, and largest has no children,
+            // so current is 2nd largest
+            if (current.right != null &&
+                    current.right.left == null &&
+                    current.right.right == null) {
+                return current.value;
+            }
+
+            current = current.right;
+        }
+    }
+
+
 }
