7e22d2a2a061416de3a9ff26bc7abb9d64c6da94
Rommel Rico <me@rommelrico.com>
2018-06-20 07:54:42 -0700

    Comments

--- java/src/com/rommelrico/exercises/s8linkedlists/deletenode/DeleteNode.java
+++ java/src/com/rommelrico/exercises/s8linkedlists/deletenode/DeleteNode.java
@@ -1,5 +1,29 @@
 package com.rommelrico.exercises.s8linkedlists.deletenode;
 
+/**
+ * Solution
+ *
+ * We take value and next from the input node's next node and copy them into the input node. Now the input node's
+ * previous node effectively skips the input node's old value!
+ *
+ * Note that this approach doesn't work for deleting the last node in the list. It also has the following side-effects:
+ * 1. Any references to the input node have now effectively been reassigned to its next node.
+ * 2. If there are pointers to the input node's original next node, those pointers now point to a "dangling" node.
+ *
+ * Complexity
+ *
+ * O(1) time and O(1) space.
+ *
+ * What We Learned
+ *
+ * My favorite part of this problem is how imperfect the solution is. Because it modifies the list "in place" it can
+ * cause other parts of the surrounding system to break. This is called a "side effect."
+ *
+ * In-place operations like this can save time and/or space, but they're risky. If you ever make in-place modifications
+ * in an interview, make sure you tell your interviewer that in a real system you'd carefully check for side effects in
+ * the rest of the code base.
+ *
+ */
 public class DeleteNode {
 
     private static void deleteNode(LinkedListNode nodeToDelete) {
