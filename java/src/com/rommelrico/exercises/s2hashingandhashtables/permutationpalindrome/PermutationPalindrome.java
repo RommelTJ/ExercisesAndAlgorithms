036f4b3a5fa704076564509c84b9de11744b5486
Rommel Rico <rommeltj@gmail.com>
2018-06-19 21:18:23 -0700

    Testing

--- java/src/com/rommelrico/exercises/s2hashingandhashtables/permutationpalindrome/PermutationPalindrome.java
+++ java/src/com/rommelrico/exercises/s2hashingandhashtables/permutationpalindrome/PermutationPalindrome.java
@@ -23,4 +23,16 @@ public class PermutationPalindrome {
         return unpairedCharacters.size() <= 1;
     }
 
+    public static void main(String[] args) {
+        String test1 = "civic";
+        String test2 = "ivicc";
+        String test3 = "civil";
+        String test4 = "livci";
+
+        System.out.println(test1 + " should return: " + hasPalindromePermutation(test1));
+        System.out.println(test2 + " should return: " + hasPalindromePermutation(test2));
+        System.out.println(test3 + " should return: " + hasPalindromePermutation(test3));
+        System.out.println(test4 + " should return: " + hasPalindromePermutation(test4));
+    }
+
 }
