47f19fa21f8718e45186a50a4977f66663cb4767
Rommel Rico <me@rommelrico.com>
2018-06-22 07:52:30 -0700

    Solution

--- java/src/com/rommelrico/exercises/s12combinatronicsprobabiblityandothermath/simulatefivesideddie/SimulateFiveSidedDie.java
+++ java/src/com/rommelrico/exercises/s12combinatronicsprobabiblityandothermath/simulatefivesideddie/SimulateFiveSidedDie.java
@@ -8,4 +8,12 @@ public class SimulateFiveSidedDie {
         return new SplittableRandom().nextInt(1, 8);
     }
 
+    public static int rand5() {
+        int result = 7;  // arbitrarily large
+        while (result > 5) {
+            result = rand7();
+        }
+        return result;
+    }
+    
 }
