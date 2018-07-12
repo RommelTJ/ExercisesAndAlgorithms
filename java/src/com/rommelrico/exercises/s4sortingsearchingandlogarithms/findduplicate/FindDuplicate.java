730a40c95882a8676c8c1613223a2c9f5729557e
Rommel Rico <rommeltj@gmail.com>
2018-07-11 20:47:46 -0700

    Comments

--- java/src/com/rommelrico/exercises/s4sortingsearchingandlogarithms/findduplicate/FindDuplicate.java
+++ java/src/com/rommelrico/exercises/s4sortingsearchingandlogarithms/findduplicate/FindDuplicate.java
@@ -1,8 +1,38 @@
 package com.rommelrico.exercises.s4sortingsearchingandlogarithms.findduplicate;
 
+/**
+ * Solution
+ *
+ * Our approach is similar to a binary search, except we divide the range of possible answers in half at each step,
+ * rather than dividing the array in half.
+ * 1. Find the number of integers in our input array which lie within the range 1..n/2.
+ * 2. Compare that to the number of possible unique integers in the same range.
+ * 3. If the number of actual integers is greater than the number of possible integers, we know there’s a duplicate
+ *    in the range 1..n/2​​, so we iteratively use the same approach on that range.
+ * 4. If the number of actual integers is not greater than the number of possible integers, we know there must be
+ *    duplicate in the range n/2 + 1..n, so we iteratively use the same approach on that range.
+ * 5. At some point, our range will contain just 1 integer, which will be our answer.
+ *
+ * Complexity
+ *
+ * O(1) space and O(n * lg n) time.
+ *
+ * What We Learned
+ *
+ * Our answer was a modified binary search. We got there by reasoning about the expected runtime:
+ * 1. We started with an O(n^2)  "brute force" solution and wondered if we could do better.
+ * 2. We knew to beat O(n^2) we'd probably do O(n) or O(n * lg n), so we started thinking of ways we might get an
+ *    O(n * lg n) runtime.
+ * 3. lg(n) usually comes from iteratively cutting stuff in half, so we arrived at the final algorithm by exploring
+ *    that idea.
+ *
+ * Starting with a target runtime and working backward from there can be a powerful strategy for all kinds of coding
+ * interview questions.
+ *
+ */
 public class FindDuplicate {
 
-    public static int findRepeat(int[] theArray) {
+    private static int findRepeat(int[] theArray) {
 
         int floor = 1;
         int ceiling = theArray.length - 1;
