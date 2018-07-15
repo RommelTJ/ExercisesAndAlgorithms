d0d1bed66e01a3059caa780d20e424106a485d37
Rommel Rico <rommeltj@gmail.com>
2018-07-15 04:12:09 -0700

    Comments

--- java/src/com/rommelrico/exercises/s2hashingandhashtables/worldclouddata/WordCloudData.java
+++ java/src/com/rommelrico/exercises/s2hashingandhashtables/worldclouddata/WordCloudData.java
@@ -3,6 +3,45 @@ package com.rommelrico.exercises.s2hashingandhashtables.worldclouddata;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Solution
+ *
+ * In our solution, we make three decisions:
+ * 1. We use a class. This allows us to tie our methods together, calling them on instances of our class instead of
+ *    passing references.
+ * 2. To handle duplicate words with different cases, we choose to make a word uppercase in our hash map only if it
+ *    is always uppercase in the original string. While this is a reasonable approach, it is imperfect (consider
+ *    proper nouns that are also lowercase words, like "Bill" and "bill").
+ * 3. We build our own splitWords() method instead of using a built-in one. This allows us to pass each word to
+ *    our addWordToHashMap() method as it was split, and to split words and eliminate punctuation in one iteration.
+ *
+ * To split the words in the input string and populate a hash map of the unique words to the number of times they
+ * occurred, we:
+ * 1. Split words by spaces, em dashes, and ellipses — making sure to include hyphens surrounded by characters. We
+ *    also include all apostrophes (which will handle contractions nicely but will break possessives into separate
+ *    words).
+ * 2. Populate the words in our hash map as they are identified, checking if the word is already in our hash map in
+ *    its current case or another case.
+ *
+ * If the input word is uppercase and there's a lowercase version in the hash map, we increment the lowercase
+ * version's count. If the input word is lowercase and there's an uppercase version in the hash map, we "demote" the
+ * uppercase version by adding the lowercase version and giving it the uppercase version's count.
+ *
+ * Complexity
+ *
+ * O(n) time, and O(n) space.
+ *
+ * What We Learned
+ *
+ * To handle capitalized words, there were lots of heuristics and approaches we could have used, each with their own
+ * strengths and weaknesses. Open-ended questions like this can really separate good engineers from great engineers.
+ *
+ * Good engineers will come up with a solution, but great engineers will come up with several solutions, weigh them
+ * carefully, and choose the best solution for the given context. So as you're running practice questions, challenge
+ * yourself to keep thinking even after you have a first solution. See how many solutions you can come up with. This
+ * will grow your ability to quickly see multiple ways to solve a problem, so you can figure out the best solution.
+ *
+ */
 public class WordCloudData {
 
     private Map<String, Integer> wordsToCounts = new HashMap<>();
