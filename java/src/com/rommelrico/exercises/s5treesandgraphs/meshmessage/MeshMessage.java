567fdcc954f3de5d9b0ec9dd1e64c8e20a7b13e8
Rommel Rico <rommeltj@gmail.com>
2018-07-22 15:46:45 -0700

    Testing

--- java/src/com/rommelrico/exercises/s5treesandgraphs/meshmessage/MeshMessage.java
+++ java/src/com/rommelrico/exercises/s5treesandgraphs/meshmessage/MeshMessage.java
@@ -68,5 +68,24 @@ public class MeshMessage {
         // so there's NO path from startNode to endNode
         return null;
     }
-    
+
+    public static void main(String[] args) {
+        Map<String, String[]> network = new HashMap<String, String[]>() {{
+            put("Min",     new String[] { "William", "Jayden", "Omar" });
+            put("William", new String[] { "Min", "Noam" });
+            put("Jayden",  new String[] { "Min", "Amelia", "Ren", "Noam" });
+            put("Ren",     new String[] { "Jayden", "Omar" });
+            put("Amelia",  new String[] { "Jayden", "Adam", "Miguel" });
+            put("Adam",    new String[] { "Amelia", "Miguel", "Sofia", "Lucas" });
+            put("Miguel",  new String[] { "Amelia", "Adam", "Liam", "Nathan" });
+            put("Noam",    new String[] { "Nathan", "Jayden", "William" });
+            put("Omar",    new String[] { "Ren", "Min", "Scott" });
+        }};
+
+        String[] path = bfsGetPath(network, "Jayden", "Adam");
+        for (String node: path) {
+            System.out.println("PATH: " + node);
+        }
+    }
+
 }
