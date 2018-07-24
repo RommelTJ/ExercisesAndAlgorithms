8810b942440d59a741fe6d49a1d9586dcaf8f084
Rommel Rico <rommeltj@gmail.com>
2018-07-23 21:27:49 -0700

    Comments

--- java/src/com/rommelrico/exercises/s5treesandgraphs/findrepeat/FindRepeat.java
+++ java/src/com/rommelrico/exercises/s5treesandgraphs/findrepeat/FindRepeat.java
@@ -1,5 +1,41 @@
 package com.rommelrico.exercises.s5treesandgraphs.findrepeat;
 
+/**
+ * Solution
+ *
+ * We treat the input array as a linked list like we described in the prompt.
+ *
+ * To find a duplicate integer:
+ * 1. We know the position of a node with multiple incoming pointers is a duplicate in our array because the nodes that
+ *    pointed to it must have the same value.
+ * 2. We find a node with multiple incoming pointers by finding the first node in a cycle.
+ * 3. We find the first node in a cycle by finding the length of the cycle and advancing two pointers: one starting at
+ *    the head of the linked list, and the other starting ahead as many steps as there are nodes in the cycle. The
+ *    pointers will meet at the first node in the cycle.
+ * 4. We find the length of a cycle by remembering a position inside the cycle and counting the number of steps it
+ *    takes to get back to that position.
+ * 5. We get inside a cycle by starting at the head and walking nnn steps. We know the head of the list is at position
+ *    n+1.
+ *
+ * We want to think of our array as a linked list but we don't want to actually use up all that space, so we traverse
+ * our array as if it were a linked list by converting positions to indices.
+ *
+ * Complexity
+ *
+ * O(n) time and O(1) space.
+ *
+ * What We Learned
+ *
+ * This one's pretty crazy. It's hard to imagine an interviewer expecting you to get all the way through this question
+ * without help.
+ *
+ * But just because it takes a few hints to get to the answer doesn't mean a question is "too hard." Some interviewers
+ * expect they'll have to offer a few hints.
+ *
+ * So if you get a hint in an interview, just relax and listen. The most impressive thing you can do is drop what
+ * you're doing, fully understand the hint, and then run with it.
+ *
+ */
 public class FindRepeat {
 
     public static int findRepeat(int[] intArray) {
