971c3a52d188624ca4e55da6065c9e1ac14bec0e
Rommel Rico <me@rommelrico.com>
2018-07-25 20:40:58 -0700

    Comments

--- java/src/com/rommelrico/exercises/s6dynamicprogrammingandrecursion/cakethief/CakeThief.java
+++ java/src/com/rommelrico/exercises/s6dynamicprogrammingandrecursion/cakethief/CakeThief.java
@@ -1,5 +1,36 @@
 package com.rommelrico.exercises.s6dynamicprogrammingandrecursion.cakethief;
 
+/**
+ * Solution
+ *
+ * This is a classic computer science puzzle called "the unbounded knapsack problem."
+ *
+ * We use a bottom-up approach to find the max value at our duffel bag's weightCapacity by finding the max value at
+ * every capacity from 0 to weightCapacity.
+ *
+ * We allocate an array maxValuesAtCapacities where the indices are capacities and each value is the max value at that
+ * capacity.
+ *
+ * For each capacity, we want to know the max monetary value we can carry. To figure that out, we go through each cake,
+ * checking to see if we should take that cake.
+ *
+ * The best monetary value we can get if we take a given cake is simply:
+ * 1. that cake's value, plus
+ * 2. the best monetary value we can carry in our remaining duffel bag capacity after taking the cake - which we'll
+ *    already have stored in maxValuesAtCapacities.
+ *
+ * To handle weights and values of zero, we throw an infinity error only if a cake weighs nothing and has a positive
+ * value.
+ *
+ * Complexity
+ *
+ * O(n*k) time, and O(k) space, where n is number of types of cake and k is the capacity of the duffel bag.
+ *
+ * What We Learned
+ *
+ * This question is our spin on the famous "unbounded knapsack problem" — a classic dynamic programming question.
+ *
+ */
 public class CakeThief {
 
     public static long maxDuffelBagValue(CakeType[] cakeTypes, int weightCapacity) {
