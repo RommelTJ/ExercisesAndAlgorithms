2589a8e2203c58ee57f4c848148a8258aa9520f7
Rommel Rico <me@rommelrico.com>
2018-07-26 20:45:54 -0700

    NodeDepthPair

---
+++ java/src/com/rommelrico/exercises/s6dynamicprogrammingandrecursion/balancedbinarytree/NodeDepthPair.java
@@ -0,0 +1,12 @@
+package com.rommelrico.exercises.s6dynamicprogrammingandrecursion.balancedbinarytree;
+
+public class NodeDepthPair {
+    BinaryTreeNode node;
+    int depth;
+
+    NodeDepthPair(BinaryTreeNode node, int depth) {
+        this.node = node;
+        this.depth = depth;
+    }
+
+}
