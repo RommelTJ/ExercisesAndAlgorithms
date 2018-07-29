a550f77c55437bb0b6d9307b848cdf2666a8e233
Rommel Rico <rommeltj@gmail.com>
2018-07-28 17:09:49 -0700

    Node Bounds

---
+++ java/src/com/rommelrico/exercises/s6dynamicprogrammingandrecursion/binarysearchtreechecker/NodeBounds.java
@@ -0,0 +1,16 @@
+package com.rommelrico.exercises.s6dynamicprogrammingandrecursion.binarysearchtreechecker;
+
+import com.rommelrico.exercises.s5treesandgraphs.binarysearchtreechecker.BinaryTreeNode;
+
+public class NodeBounds {
+
+    BinaryTreeNode node;
+    int lowerBound;
+    int upperBound;
+
+    NodeBounds(BinaryTreeNode node, int lowerBound, int upperBound) {
+        this.node = node;
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    }
+}
