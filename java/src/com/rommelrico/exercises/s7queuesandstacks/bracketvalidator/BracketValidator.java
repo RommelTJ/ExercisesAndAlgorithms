72620d1e212d5cdfd9adb676706e8da5ae703068
Rommel Rico <rommeltj@gmail.com>
2018-08-04 05:46:41 -0700

    Solution

---
+++ java/src/com/rommelrico/exercises/s7queuesandstacks/bracketvalidator/BracketValidator.java
@@ -0,0 +1,44 @@
+package com.rommelrico.exercises.s7queuesandstacks.bracketvalidator;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class BracketValidator {
+
+    public static boolean isValid(String code) {
+
+        Map<Character, Character> openersToClosers = new HashMap<>();
+        openersToClosers.put('(', ')');
+        openersToClosers.put('[', ']');
+        openersToClosers.put('{', '}');
+
+        Set<Character> openers = openersToClosers.keySet();
+        Set<Character> closers = new HashSet<>(openersToClosers.values());
+
+        Deque<Character> openersStack = new ArrayDeque<>();
+
+        for (char c : code.toCharArray()) {
+            if (openers.contains(c)) {
+                openersStack.push(c);
+            } else if (closers.contains(c)) {
+                if (openersStack.isEmpty()) {
+                    return false;
+                } else {
+                    char lastUnclosedOpener = openersStack.pop();
+
+                    // if this closer doesn't correspond to the most recently
+                    // seen unclosed opener, short-circuit, returning false
+                    if (openersToClosers.get(lastUnclosedOpener) != c) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return openersStack.isEmpty();
+    }
+
+}
