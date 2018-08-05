b859f3c7b2b939eb9f4cd4c8a31ea0062a87aa2d
Rommel Rico <me@rommelrico.com>
2018-08-05 12:50:20 -0700

    Testing

--- java/src/com/rommelrico/exercises/s8linkedlists/reverselinkedlist/ReverseLinkedList.java
+++ java/src/com/rommelrico/exercises/s8linkedlists/reverselinkedlist/ReverseLinkedList.java
@@ -1,5 +1,7 @@
 package com.rommelrico.exercises.s8linkedlists.reverselinkedlist;
 
+import java.util.LinkedList;
+
 public class ReverseLinkedList {
 
     public static LinkedListNode reverse(LinkedListNode headOfList) {
@@ -23,6 +25,24 @@ public class ReverseLinkedList {
         }
 
         return previousNode;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode node1 = new LinkedListNode(1);
+        LinkedListNode node2 = new LinkedListNode(2);
+        LinkedListNode node3 = new LinkedListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+
+        System.out.println("node1 = " + node1.toString());
+        System.out.println("node2 = " + node2.toString());
+        System.out.println("node3 = " + node3.toString());
+
+        reverse(node1);
+
+        System.out.println("node1 = " + node1.toString());
+        System.out.println("node2 = " + node2.toString());
+        System.out.println("node3 = " + node3.toString());
     }
 
 }
