df300904afb5054d1d24ddfba741e93dd4f17ba8
Rommel Rico <rommeltj@gmail.com>
2018-08-12 14:58:23 -0700

    Testing

--- java/src/com/rommelrico/exercises/s11bitmanipulation/stolenbreakfastdrone/BreakfastDrone.java
+++ java/src/com/rommelrico/exercises/s11bitmanipulation/stolenbreakfastdrone/BreakfastDrone.java
@@ -13,4 +13,9 @@ public class BreakfastDrone {
         return uniqueDeliveryId;
     }
 
+    public static void main(String[] args) {
+        int[] deliveryIds = {1, 2, 3, 4, 5, 3, 2, 4, 1};
+        System.out.println("Unique Delivery ID: " + findUniqueDeliveryId(deliveryIds));
+    }
+
 }
