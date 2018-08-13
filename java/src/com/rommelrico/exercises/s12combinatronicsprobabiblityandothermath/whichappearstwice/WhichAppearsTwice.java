a6194cc0f21bb0cf2951e2ed0df042165d166e28
Rommel Rico <rommeltj@gmail.com>
2018-08-13 16:10:58 -0700

    Comments

--- java/src/com/rommelrico/exercises/s12combinatronicsprobabiblityandothermath/whichappearstwice/WhichAppearsTwice.java
+++ java/src/com/rommelrico/exercises/s12combinatronicsprobabiblityandothermath/whichappearstwice/WhichAppearsTwice.java
@@ -1,5 +1,19 @@
 package com.rommelrico.exercises.s12combinatronicsprobabiblityandothermath.whichappearstwice;
 
+/**
+ * Solution
+ *
+ * First, we sum all numbers 1..n. We can do this using the equation: (n^2 + n) / 2.
+ * because the numbers in 1..n are a triangular series.
+ *
+ * Second, we sum all numbers in our input array, which should be the same as our other sum but with our repeat number
+ * added in twice. So the difference between these two sums is the repeated number!
+ *
+ * Complexity
+ *
+ * O(n) time and O(1) space.
+ *
+ */
 public class WhichAppearsTwice {
 
     public static int findRepeat(int[] numbers) {
