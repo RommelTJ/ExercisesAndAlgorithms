b71ecbac64e216aa64244a9a9d79d3937d64c4eb
Rommel Rico <me@rommelrico.com>
2019-04-02 07:04:16 -0700

    Clown implementation

--- java/src/com/rommelrico/designpatterns/strategy/strategyone/model/Clown.java
+++ java/src/com/rommelrico/designpatterns/strategy/strategyone/model/Clown.java
@@ -1,4 +1,12 @@
 package com.rommelrico.designpatterns.strategy.strategyone.model;
 
-public class Clown {
+import com.rommelrico.designpatterns.strategy.strategyone.controllers.*;
+
+public class Clown extends ScoreAlgorithmBase {
+
+    @Override
+    public int calculateScore(int taps, int multiplier) {
+        return (taps * multiplier) - 10;
+    }
+
 }
