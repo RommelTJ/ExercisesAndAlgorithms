cc17f5b7836ae299009278e56d2a6c0dd7d8fe38
Rommel Rico <me@rommelrico.com>
2019-04-02 07:08:49 -0700

    Testing the ScoreBoard

--- java/src/com/rommelrico/designpatterns/strategy/strategyone/controllers/ScoreBoard.java
+++ java/src/com/rommelrico/designpatterns/strategy/strategyone/controllers/ScoreBoard.java
@@ -2,7 +2,7 @@ package com.rommelrico.designpatterns.strategy.strategyone.controllers;
 
 public class ScoreBoard {
 
-    private ScoreAlgorithmBase algorithmBase;
+    public ScoreAlgorithmBase algorithmBase;
 
     public void showScore(int taps, int multiplier) {
         System.out.println(algorithmBase.calculateScore(taps, multiplier));
