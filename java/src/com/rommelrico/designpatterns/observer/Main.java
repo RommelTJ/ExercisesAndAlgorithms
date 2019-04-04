7592ba3c1fa2de3db720525232e51a934a1a0695
Rommel Rico <me@rommelrico.com>
2019-04-04 06:56:17 -0700

    Testing

---
+++ java/src/com/rommelrico/designpatterns/observer/Main.java
@@ -0,0 +1,33 @@
+package com.rommelrico.designpatterns.observer;
+
+import com.rommelrico.designpatterns.observer.interfaces.*;
+import com.rommelrico.designpatterns.observer.models.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EmailTopic emailTopic = new EmailTopic();
+
+        // Create Observers
+        Observer firstObserver = new EmailTopicSubscriber("FirstObserver");
+        Observer secondObserver = new EmailTopicSubscriber("SecondObserver");
+        Observer thirdObserver = new EmailTopicSubscriber("ThirdObserver");
+
+        // Register Observers
+        emailTopic.register(firstObserver);
+        emailTopic.register(secondObserver);
+        emailTopic.register(thirdObserver);
+
+        // Attaching observer to subject
+        firstObserver.setSubject(emailTopic);
+        secondObserver.setSubject(emailTopic);
+        thirdObserver.setSubject(emailTopic);
+
+        // Check for updates
+        firstObserver.update();
+
+        // Provider/Subject (broadcaster)
+        emailTopic.postMessage("Hello Subscribers");
+
+    }
+}
