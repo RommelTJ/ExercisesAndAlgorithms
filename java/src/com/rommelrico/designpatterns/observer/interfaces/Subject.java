4ee6d76a4365584cd36ae28881aabb7fae5c479d
Rommel Rico <me@rommelrico.com>
2019-04-04 06:38:36 -0700

    The Subject interface

---
+++ java/src/com/rommelrico/designpatterns/observer/interfaces/Subject.java
@@ -0,0 +1,8 @@
+package com.rommelrico.designpatterns.observer.interfaces;
+
+public interface Subject {
+    void register(Observer observer);
+    void unregister(Observer observer);
+    void notifyObservers();
+    Object getUpdate(Observer observer);
+}
