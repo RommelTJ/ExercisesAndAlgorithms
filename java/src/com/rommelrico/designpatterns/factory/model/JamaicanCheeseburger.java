eccddec4fb5e85ecb9e94aa501a834497ed2ee96
Rommel Rico <me@rommelrico.com>
2019-04-06 08:42:32 -0700

    Jamaican Cheeseburger

---
+++ java/src/com/rommelrico/designpatterns/factory/model/JamaicanCheeseburger.java
@@ -0,0 +1,9 @@
+package com.rommelrico.designpatterns.factory.model;
+
+public class JamaicanCheeseburger extends Hamburger {
+
+    public JamaicanCheeseburger(String name) {
+        super(name);
+    }
+
+}
