5c49ce45b770b77aa53e802220ce16e50ca8fd62
Rommel Rico <me@rommelrico.com>
2019-04-06 09:02:35 -0700

    Testing

--- java/src/com/rommelrico/designpatterns/factory/Main.java
+++ java/src/com/rommelrico/designpatterns/factory/Main.java
@@ -1,8 +1,18 @@
 package com.rommelrico.designpatterns.factory;
 
+import com.rommelrico.designpatterns.factory.interfaces.*;
+import com.rommelrico.designpatterns.factory.model.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        HamburgerStore mozambicanStore = new MozambicanHamburgerStore();
+        HamburgerStore jamaicanStore = new JamaicanHamburgerStore();
+
+        Hamburger burger1 = mozambicanStore.orderHamburger("cheese");
+        System.out.println("Rommel ordered: " + burger1.getName());
+
+        Hamburger burger2 = jamaicanStore.orderHamburger("veggie");
+        System.out.println("Rommel ordered: " + burger2.getName());
     }
 }
