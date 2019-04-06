d41863b998c294e6d1a9a4a656119f5bc941ceee
Rommel Rico <me@rommelrico.com>
2019-04-06 08:42:40 -0700

    Jamaican Veggieburger

---
+++ java/src/com/rommelrico/designpatterns/factory/model/JamaicanVeggieburger.java
@@ -0,0 +1,9 @@
+package com.rommelrico.designpatterns.factory.model;
+
+public class JamaicanVeggieburger extends Hamburger {
+
+    public JamaicanVeggieburger(String name) {
+        super(name);
+    }
+
+}
