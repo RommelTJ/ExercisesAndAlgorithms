b0bf83b85fcb963e152c7a4d20b358fd979958d1
Rommel Rico <me@rommelrico.com>
2019-04-06 08:49:23 -0700

    Mozambican Veggieburger

---
+++ java/src/com/rommelrico/designpatterns/factory/model/MozambicanVeggieburger.java
@@ -0,0 +1,9 @@
+package com.rommelrico.designpatterns.factory.model;
+
+class MozambicanVeggieburger extends Hamburger {
+
+    MozambicanVeggieburger(String name) {
+        super(name);
+    }
+
+}
