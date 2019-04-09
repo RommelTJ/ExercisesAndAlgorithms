6c8442d89ee14ff76fedcb4a167f1aac39672489
Rommel Rico <me@rommelrico.com>
2019-04-09 07:09:20 -0700

    Verifying we're only creating one instance of singleton

--- java/src/com/rommelrico/designpatterns/singleton/Main.java
+++ java/src/com/rommelrico/designpatterns/singleton/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         MyClass myClass = MyClass.getInstance();
+        MyClass secondClass = MyClass.getInstance();
         myClass.setName("My Class");
         System.out.println(myClass.getName());
+        System.out.println(myClass);
+        System.out.println(secondClass);
+
+        Person person = new Person();
+        Person secondPerson = new Person();
+        System.out.println("Object person: " + person);
+        System.out.println("Object person: " + secondPerson);
     }
 }
