cc3e5b8c3055c97503bff8aa57d0619f8eaf98c5
Rommel Rico <me@rommelrico.com>
2019-04-10 07:05:36 -0700

    Double-check locking

--- java/src/com/rommelrico/designpatterns/singleton/Singleton.java
+++ java/src/com/rommelrico/designpatterns/singleton/Singleton.java
@@ -2,12 +2,20 @@ package com.rommelrico.designpatterns.singleton;
 
 public class Singleton {
 
-    private static Singleton uniqueInstance = new Singleton();
-    String name;
+    // Volatile = ensures multiple thread checking
+    private volatile static Singleton uniqueInstance;
 
     private Singleton() {}
 
+    // Double-check locking
     public static Singleton getInstance() {
+        if (uniqueInstance == null) {
+            synchronized ((Singleton.class)) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
         return uniqueInstance;
     }
 
