57f9dde4a208d2c650a277dd275896388bc2a821
Rommel Rico <me@rommelrico.com>
2019-04-10 06:59:06 -0700

    Eager instantiation

--- java/src/com/rommelrico/designpatterns/singleton/MyClass.java
+++ java/src/com/rommelrico/designpatterns/singleton/MyClass.java
@@ -2,17 +2,20 @@ package com.rommelrico.designpatterns.singleton;
 
 class MyClass {
 
-    private static MyClass uniqueInstance;
+    private static MyClass uniqueInstance = new MyClass();
 
     String name;
 
     private MyClass() { }
 
-    static synchronized MyClass getInstance() {
-        // Adding the synchronized keyword makes our singleton thread safe
-        if (uniqueInstance == null) {
-            uniqueInstance = new MyClass();
-        }
+//    static synchronized MyClass getInstance() {
+//        // Adding the synchronized keyword makes our singleton thread safe
+//        if (uniqueInstance == null) {
+//            uniqueInstance = new MyClass();
+//        }
+
+    // Eager Singleton
+    static MyClass getInstance() {
         return uniqueInstance;
     }
 
