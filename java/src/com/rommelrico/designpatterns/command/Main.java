5a97fac1b1e6604a4c767aa1a25c819a91c4b300
Rommel Rico <me@rommelrico.com>
2019-04-13 20:13:24 -0700

    Minor: Testing

--- java/src/com/rommelrico/designpatterns/command/Main.java
+++ java/src/com/rommelrico/designpatterns/command/Main.java
@@ -1,9 +1,35 @@
 package com.rommelrico.designpatterns.command;
 
+import com.rommelrico.designpatterns.command.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Receivers
+        MarioCharacterReceiver mario = new MarioCharacterReceiver();
+        mario.setName("Mario");
 
+        KirbyCharacterReceiver kirby = new KirbyCharacterReceiver();
+        kirby.setName("Kirby");
+
+        // Commands
+        MarioUpCommand marioUpCommand = new MarioUpCommand(mario);
+        MarioDownCommand marioDownCommand = new MarioDownCommand(mario);
+        MarioLeftCommand marioLeftCommand = new MarioLeftCommand(mario);
+        MarioRightCommand marioRightCommand = new MarioRightCommand(mario);
+
+        KirbyUpCommand kirbyUpCommand = new KirbyUpCommand(kirby);
+        KirbyDownCommand kirbyDownCommand = new KirbyDownCommand(kirby);
+        KirbyLeftCommand kirbyLeftCommand = new KirbyLeftCommand(kirby);
+        KirbyRightCommand kirbyRightCommand = new KirbyRightCommand(kirby);
+
+        // Invoker
+        GameBoy gameBoy = new GameBoy(marioUpCommand, marioDownCommand, marioLeftCommand, marioRightCommand);
+        GameBoy gameBoy2 = new GameBoy(kirbyUpCommand, kirbyDownCommand, kirbyLeftCommand, kirbyRightCommand);
+
+        gameBoy.arrowUp();
+        gameBoy2.arrowUp();
+        gameBoy.arrowRight();
     }
 
 }
