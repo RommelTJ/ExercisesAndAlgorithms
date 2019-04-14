aea67ff8bac9895013ccec6cf92b74f29436e704
Rommel Rico <me@rommelrico.com>
2019-04-14 08:53:16 -0700

    feat(Main): getVolt helper

--- java/src/com/rommelrico/designpatterns/adapter/Main.java
+++ java/src/com/rommelrico/designpatterns/adapter/Main.java
@@ -1,9 +1,23 @@
 package com.rommelrico.designpatterns.adapter;
 
+import com.rommelrico.designpatterns.adapter.interfaces.*;
+import com.rommelrico.designpatterns.adapter.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    private static Volt getVolt(SocketAdapter socketAdapter, int i) {
+        switch (i) {
+            case 3:
+                return socketAdapter.get3Volts();
+            case 12:
+                return socketAdapter.get12Volts();
+            default:
+                return socketAdapter.get120Volts();
+        }
     }
 
 }
