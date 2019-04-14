d4bc607e76c744ea318669b32e0515dc875e52ae
Rommel Rico <me@rommelrico.com>
2019-04-14 08:55:33 -0700

    feat(Main): Testing

--- java/src/com/rommelrico/designpatterns/adapter/Main.java
+++ java/src/com/rommelrico/designpatterns/adapter/Main.java
@@ -6,7 +6,7 @@ import com.rommelrico.designpatterns.adapter.models.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        testingObjectAdapter();
     }
 
     private static void testingObjectAdapter() {
