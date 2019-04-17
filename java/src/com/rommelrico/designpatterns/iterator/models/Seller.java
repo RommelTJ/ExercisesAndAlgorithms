3f0b53ee185ce5cea39b856ea5bd59343f6ed754
Rommel Rico <me@rommelrico.com>
2019-04-16 20:13:14 -0700

    feat(Seller): Adding methods for printing catalogs

--- java/src/com/rommelrico/designpatterns/iterator/models/Seller.java
+++ java/src/com/rommelrico/designpatterns/iterator/models/Seller.java
@@ -5,14 +5,27 @@ import com.rommelrico.designpatterns.iterator.*;
 public class Seller {
 
     GeekyStoreCatalog geekyStoreCatalog;
+    DevStoreCatalog devStoreCatalog;
 
-    public Seller(GeekyStoreCatalog geekyStoreCatalog) {
+    public Seller(GeekyStoreCatalog geekyStoreCatalog, DevStoreCatalog devStoreCatalog) {
         this.geekyStoreCatalog = geekyStoreCatalog;
+        this.devStoreCatalog = devStoreCatalog;
     }
 
-    public void printCatalog() {
+    public void printGeekyCatalog() {
         GeekyStoreIterator iterator = geekyStoreCatalog.createIterator();
-        System.out.println("Printing catalog: ");
+        System.out.println("Printing geeky store catalog: ");
+        while (iterator.hasNext()) {
+            Product product = (Product) iterator.next();
+            System.out.println("Product: { name: " + product.getName()
+                    + ", description: " + product.getDescription()
+                    + ", price: " + product.getPrice() + " }" );
+        }
+    }
+
+    public void printDevCatalog() {
+        DevStoreIterator iterator = devStoreCatalog.createIterator();
+        System.out.println("Printing dev store catalog: ");
         while (iterator.hasNext()) {
             Product product = (Product) iterator.next();
             System.out.println("Product: { name: " + product.getName()
