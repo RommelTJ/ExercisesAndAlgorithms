e5129411cd1fa069b3f651cdde5104e5cdd64302
Rommel Rico <me@rommelrico.com>
2019-04-17 07:06:40 -0700

    feat(Main): Testing

--- java/src/com/rommelrico/designpatterns/statedesign/Main.java
+++ java/src/com/rommelrico/designpatterns/statedesign/Main.java
@@ -1,9 +1,28 @@
 package com.rommelrico.designpatterns.statedesign;
 
+import com.rommelrico.designpatterns.statedesign.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        SodaMachine sodaMachine = new SodaMachine(3);
+
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+
+        sodaMachine.insertMoney();
+        sodaMachine.ejectMoney();
+
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+
+        sodaMachine.insertMoney();
+        sodaMachine.ejectMoney();
+
+        sodaMachine.selectSoda();
+        sodaMachine.insertMoney();
+        sodaMachine.selectSoda();
+        sodaMachine.insertMoney();
     }
 
 }
