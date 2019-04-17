54db8eb9bfb8834cef2162fcd5084b165db38f75
Rommel Rico <me@rommelrico.com>
2019-04-16 20:36:28 -0700

    feat(Main): Testing

--- java/src/com/rommelrico/designpatterns/iterator/Main.java
+++ java/src/com/rommelrico/designpatterns/iterator/Main.java
@@ -1,12 +1,13 @@
 package com.rommelrico.designpatterns.iterator;
 
+import com.rommelrico.designpatterns.iterator.interfaces.*;
 import com.rommelrico.designpatterns.iterator.models.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        DevStoreCatalog devStoreCatalog = new DevStoreCatalog();
-        GeekyStoreCatalog geekyStoreCatalog = new GeekyStoreCatalog();
+        Catalog devStoreCatalog = new DevStoreCatalog();
+        Catalog geekyStoreCatalog = new GeekyStoreCatalog();
 
         Seller seller = new Seller(geekyStoreCatalog, devStoreCatalog);
         seller.printGeekyCatalog();
