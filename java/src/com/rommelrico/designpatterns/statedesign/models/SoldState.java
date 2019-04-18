a836279560a432050c93f5d8bcde823cfb8fe30a
Rommel Rico <me@rommelrico.com>
2019-04-18 07:12:18 -0700

    feat(State): Initial scaffolding for State implementations

---
+++ java/src/com/rommelrico/designpatterns/statedesign/models/SoldState.java
@@ -0,0 +1,26 @@
+package com.rommelrico.designpatterns.statedesign.models;
+
+import com.rommelrico.designpatterns.statedesign.interfaces.*;
+
+public class SoldState implements State {
+
+    @Override
+    public void insertMoney() {
+
+    }
+
+    @Override
+    public void ejectMoney() {
+
+    }
+
+    @Override
+    public void select() {
+
+    }
+
+    @Override
+    public void dispense() {
+
+    }
+}
