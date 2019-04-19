7ca51abd989ed0d48c35fea50b0698824476a77a
Rommel Rico <me@rommelrico.com>
2019-04-19 07:03:33 -0700

    feat(SodaVendingMachine): Initial implementation

---
+++ java/src/com/rommelrico/designpatterns/statedesign/models/SodaVendingMachine.java
@@ -0,0 +1,19 @@
+package com.rommelrico.designpatterns.statedesign.models;
+
+import com.rommelrico.designpatterns.statedesign.interfaces.*;
+
+public class SodaVendingMachine {
+
+    private State soldOutState;
+    private State noMoneyState;
+    private State hasMoneyState;
+    private State soldState;
+
+    private State state = soldOutState;
+    private int count = 0;
+
+    public SodaVendingMachine(int count) {
+        soldOutState = new SoldOutState();
+        this.count = count;
+    }
+}
