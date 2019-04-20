5fa4ee5083660f92a76f67ed24a20e01479d69ba
Rommel Rico <me@rommelrico.com>
2019-04-19 19:51:27 -0700

    feat(NoMoneyState): No money state

--- java/src/com/rommelrico/designpatterns/statedesign/models/NoMoneyState.java
+++ java/src/com/rommelrico/designpatterns/statedesign/models/NoMoneyState.java
@@ -6,28 +6,33 @@ public class NoMoneyState implements State {
 
     private SodaVendingMachine sodaVendingMachine;
 
-    public NoMoneyState(SodaVendingMachine sodaVendingMachine) {
+    NoMoneyState(SodaVendingMachine sodaVendingMachine) {
         this.sodaVendingMachine = sodaVendingMachine;
     }
 
     @Override
     public void insertMoney() {
-
+        System.out.println("You inserted a dollar");
+        sodaVendingMachine.setState(sodaVendingMachine.getHasMoneyState());
     }
 
     @Override
     public void ejectMoney() {
-
+        System.out.println("You haven't inserted a dollar");
     }
 
     @Override
     public void select() {
-
+        System.out.println("You selected a soda, but there's no money");
     }
 
     @Override
     public void dispense() {
-
+        System.out.println("You need to add money first");
     }
 
+    @Override
+    public String toString() {
+        return "No Money";
+    }
 }
