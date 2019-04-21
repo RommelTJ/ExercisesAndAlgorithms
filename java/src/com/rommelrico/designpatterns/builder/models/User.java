183b5afbf3fa417b727568d61f53e8cb8d0f161b
Rommel Rico <me@rommelrico.com>
2019-04-21 15:38:07 -0700

    feat(User): Model definition

---
+++ java/src/com/rommelrico/designpatterns/builder/models/User.java
@@ -0,0 +1,13 @@
+package com.rommelrico.designpatterns.builder.models;
+
+public class User {
+
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final String phoneNumber;
+    private final String address;
+
+    
+
+}
