918b6ef44e5ed0c70c1aa4c77cddd8a74e4cd655
Rommel Rico <me@rommelrico.com>
2019-04-21 15:41:17 -0700

    feat(User): UserBuilder inner class

--- java/src/com/rommelrico/designpatterns/builder/models/User.java
+++ java/src/com/rommelrico/designpatterns/builder/models/User.java
@@ -8,6 +8,36 @@ public class User {
     private final String phoneNumber;
     private final String address;
 
-    
+    public static class UserBuilder {
+        private final String firstName; // required
+        private final String lastName; // required
+        private int age;
+        private String phoneNumber;
+        private String address;
+
+        public UserBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public UserBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public UserBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 
 }
