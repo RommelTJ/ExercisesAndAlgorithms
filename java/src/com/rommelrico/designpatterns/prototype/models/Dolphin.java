2772b5448f3f1255f54c7faf0308f7bef9ad70ef
Rommel Rico <me@rommelrico.com>
2019-04-22 07:04:43 -0700

    feat(Dolphin): Model

---
+++ java/src/com/rommelrico/designpatterns/prototype/models/Dolphin.java
@@ -0,0 +1,20 @@
+package com.rommelrico.designpatterns.prototype.models;
+
+import com.rommelrico.designpatterns.prototype.interfaces.*;
+
+public class Dolphin implements Prototype {
+
+    private String name;
+    private int age;
+
+    public Dolphin(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public Prototype clone() {
+        return new Dolphin(name, age);
+    }
+    
+}
