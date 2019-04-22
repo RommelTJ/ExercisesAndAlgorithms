3c31ea6dfe1ab5f8a942c2a1935591d4df577dce
Rommel Rico <me@rommelrico.com>
2019-04-22 07:11:46 -0700

    feat(Dolphin): Getter/setters

--- java/src/com/rommelrico/designpatterns/prototype/models/Dolphin.java
+++ java/src/com/rommelrico/designpatterns/prototype/models/Dolphin.java
@@ -17,6 +17,22 @@ public class Dolphin implements Prototype {
         return new Dolphin(name, age);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Dolphin{" +
