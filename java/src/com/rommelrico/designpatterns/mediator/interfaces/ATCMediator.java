dc03beaab739fab90bf628bf3bff35a3b5edad72
Rommel Rico <me@rommelrico.com>
2019-04-24 07:04:49 -0700

    feat(ATCMediator): Add Aircraft

--- java/src/com/rommelrico/designpatterns/mediator/interfaces/ATCMediator.java
+++ java/src/com/rommelrico/designpatterns/mediator/interfaces/ATCMediator.java
@@ -3,5 +3,6 @@ package com.rommelrico.designpatterns.mediator.interfaces;
 public interface ATCMediator {
 
     void sendMessage(String msg, Aircraft aircraft);
+    void addAircraft(Aircraft aircraft);
 
 }
