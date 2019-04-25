7757cf8adce136e61f8ddb4a05ee00a3ea21a3a6
Rommel Rico <me@rommelrico.com>
2019-04-25 07:26:19 -0700

    feat(Main): Testing

--- java/src/com/rommelrico/designpatterns/visitor/Main.java
+++ java/src/com/rommelrico/designpatterns/visitor/Main.java
@@ -1,9 +1,18 @@
 package com.rommelrico.designpatterns.visitor;
 
+import com.rommelrico.designpatterns.visitor.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Shirt shirt = new Shirt(18.99);
+        TShirt tshirt = new TShirt(9.99);
+        Jacket jacket = new Jacket(60);
+
+        TaxVisitor taxVisitor = new TaxVisitor();
+        tshirt.accept(taxVisitor);
+        shirt.accept(taxVisitor);
+        jacket.accept(taxVisitor);
     }
 
 }
