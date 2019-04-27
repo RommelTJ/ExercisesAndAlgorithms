652808843ec144ed6061e4c073711d8a2087e19c
Rommel Rico <me@rommelrico.com>
2019-04-27 11:35:27 -0700

    feat(Memento): Memento model

---
+++ java/src/com/rommelrico/designpatterns/memento/models/Memento.java
@@ -0,0 +1,13 @@
+package com.rommelrico.designpatterns.memento.models;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
