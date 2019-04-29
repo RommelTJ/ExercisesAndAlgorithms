2295e75e01779fca2e9d7c127d9b851742a376af
Rommel Rico <me@rommelrico.com>
2019-04-29 06:49:49 -0700

    feat(Vehicle): Initial implementation

---
+++ java/src/com/rommelrico/designpatterns/bridge/interfaces/Vehicle.java
@@ -0,0 +1,15 @@
+package com.rommelrico.designpatterns.bridge.interfaces;
+
+public abstract class Vehicle {
+
+    private Workshop workshop;
+    private Workshop workshop2;
+
+    public Vehicle(Workshop workshop, Workshop workshop2) {
+        this.workshop = workshop;
+        this.workshop2 = workshop2;
+    }
+
+    abstract public void manufacture();
+
+}
